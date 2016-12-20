014f59f58f3e71c743d3120154fdf2b3748cfb7f
Nathan <nathan0812@gmail.com>
2016-12-20 09:23:46 +0200

    css changes.

--- templates/menu.php
+++ templates/menu.php
@@ -5,22 +5,21 @@
                 <img src="../img/logo-v2.png" alt="הוט פלנט" title="הוט פלנט"/>
             </a>
         </div>
-        <ul>
-            <li><a href="index.php">דף הבית</a></li>
+            <ul>
+                <li><a href="index.php">דף הבית</a></li>
 
-            <?php if(!$_SESSION['LOGIN']) { ?>
-                <li><a href='register.php'>הרשמה</a></li>
-                <li><a href='login.php'>התחברות</a></li>
-            <?php } else {
-                if ($_SESSION["UserName"]->admin) {?>
-                    <li><a href="admin.php">נהל משתמשים</a></li>
+                <?php if(!$_SESSION['LOGIN']) { ?>
+                    <li><a href='register.php'>הרשמה</a></li>
+                    <li><a href='login.php'>התחברות</a></li>
+                <?php } else {
+                    if ($_SESSION["UserName"]->admin) {?>
+                        <li><a href="admin.php">נהל משתמשים</a></li>
+                    <?php } ?>
+                        <li><a href='logout.php'>התנתק</a></li>
                 <?php } ?>
-                    <li><a href='logout.php'>התנתק</a></li>
-            <?php } ?>
-            <li><a href="newmovies.php">סרטים חדשים</a></li>
-            <li><a href="contact.php">צור קשר</a></li>
-        </ul>
-
+                <li><a href="newmovies.php">סרטים חדשים</a></li>
+                <li><a href="contact.php">צור קשר</a></li>
+            </ul>
         <div class="clear"></div>
     </div>
 </header>
